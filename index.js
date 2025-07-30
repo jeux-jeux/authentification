4c15a95e896147e649ed88f6a8c85a81e0d69094
Daniel-Ersen <danielersen@icloud.com>
2025-07-30 04:50:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ app.use(express.json());
 
 // Configuration
 const PORT = process.env.PORT || 3000;
-const ALLOWED_ORIGIN = 'https://le-jeu/';
+const ALLOWED_ORIGIN = 'https://daniel-ersen.github.io/jeu-des-trizos/';
 const SECRET_KEY = '4927-3047-4398-3947-0274';
 
 // Route GET « / »
 app.get('/', (req, res) => {
   const origin = req.get('Origin') || '';
   if (origin === ALLOWED_ORIGIN) {
-    return res.json({ url: 'daniel' });
+    return res.json({ url: 'https://serveur-stockage-jeu-default-rtdb.europe-west1.firebasedatabase.app/users' });
   }
   return res.status(403).json({ message: 'Accès refusé' });
 });
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const { cle } = req.body;
   if (cle === SECRET_KEY) {
-    return res.json({ url: 'daniel' });
+    return res.json({ url: 'https://serveur-stockage-jeu-default-rtdb.europe-west1.firebasedatabase.app/users' });
   }
   return res.status(403).json({ message: 'Accès refusé' });
 });

ae087213a313f0ffc746c06fdc8e86049f060fb1
Daniel-Ersen <danielersen@icloud.com>
2025-08-02 05:55:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors());
 // GET /
 app.get('/', (req, res) => {
   const origin = req.get('Origin') || '';
-  if (origin === 'https://daniel-ersen.github.io/jeu-des-trizos/') {
+  if (origin === 'https://daniel-ersen.github.io') {
     return res.json({ url: 'https://serveur-stockage-jeu-default-rtdb.europe-west1.firebasedatabase.app' });
   }
   return res.status(403).json({ message: 'Accès refusé' });
